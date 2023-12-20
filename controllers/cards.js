135daac56ac61a7e2f114b13b61f98804b063fd6
Ivan Lev <fanatos@mail.ru>
2023-12-20 16:37:04 +0500

    feat: add card controllers

---
+++ controllers/cards.js
@@ -0,0 +1,28 @@
+const Card = require("../models/cards");
+
+module.exports.getCardsList = (req, res) => {
+  Card.find({})
+    .then((cards) => res.send({ data: cards }))
+    .catch((error) =>
+      res.status(500).send({ message: `Произошла ошибка: ${error}` })
+    );
+};
+
+module.exports.createCard = (req, res) => {
+  const { name, link, owner = req.user._id } = req.body;
+
+  Card.create({ name, link, owner })
+    .then((card) => res.send({ data: card }))
+    .catch((error) =>
+      res.status(500).send({ message: `Произошла ошибка: ${error}` })
+    );
+};
+
+module.exports.deleteCard = (req, res) => {
+  console.log(req.params.cardId);
+  Card.findByIdAndDelete(req.params.cardId)
+    .then((card) => res.send({ data: card }))
+    .catch((error) =>
+      res.status(500).send({ message: `Произошла ошибка: ${error}` })
+    );
+};
