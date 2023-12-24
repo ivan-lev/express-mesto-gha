f3a54a1df09604b5f1603679cd0bf0edf26fb30a
Ivan Lev <fanatos@mail.ru>
2023-12-24 23:06:10 +0500

    fix: made proper likes field type in card schema

--- models/card.js
+++ models/card.js
@@ -7,18 +7,25 @@ const cardSchema = new mongoose.Schema({
     minlength: 2,
     maxlength: 30,
   },
+
   link: { type: String, required: true },
+
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
     required: true,
   },
-  likes: [
-    {
-      type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
-      default: [],
-    },
-  ],
+
+  likes: {
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+    default: [],
+  },
+
   createdAt: {
     type: Date,
     default: Date.now,
