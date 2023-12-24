24d7f9317d750478e40e22546a7e3b29ac47703c
Ivan Lev <fanatos@mail.ru>
2023-12-24 23:37:58 +0500

    feat: add validator for link checking in schemas

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -13,7 +14,14 @@ const userSchema = new mongoose.Schema({
     minlength: [2, 'Минимальная длина поля "name" - 2'],
     maxlength: [30, 'Максимальная длина поля "name" - 30'],
   },
-  avatar: { type: String, required: true },
+  avatar: {
+    type: String,
+    required: [true, 'Поле "avatar" должно быть заполнено'],
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL',
+    },
+  },
 });
 
 module.exports = mongoose.model('user', userSchema);
