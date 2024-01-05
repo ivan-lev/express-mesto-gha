64013f27aec1d38998199fe72ab2e0d24deeeab3
Ivan Lev <fanatos@mail.ru>
2024-01-05 20:58:30 +0500

    feat: add email and password to schema and controller

--- controllers/users.js
+++ controllers/users.js
@@ -37,14 +37,18 @@ module.exports.getUserById = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
 
-  User.create({ name, about, avatar })
+  User.create({
+    name, about, avatar, email, password,
+  })
     .then((user) => {
       const userId = user._id.toString();
       res.status(201).send({
         data: {
-          name, about, avatar, _id: userId,
+          name, about, avatar, _id: userId, password,
         },
       });
     })
