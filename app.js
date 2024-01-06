6cb1f574f3fb355eed01e2aaa29f9421c009a221
Ivan Lev <fanatos@mail.ru>
2024-01-06 20:05:21 +0500

    feat: add auth middlware and remove hardcoded user._id

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+
 const userRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const pageNotFound = require('./routes/page-not-found');
+
 const { createUser, login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 
@@ -13,22 +16,26 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-const tempUserIdInsertion = (req, res, next) => {
-  req.user = {
-    _id: '6582a0da482a2f0244ac128a',
-  };
+// const tempUserIdInsertion = (req, res, next) => {
+//   req.user = {
+//     _id: '6582a0da482a2f0244ac128a',
+//   };
 
-  next();
-};
+//   next();
+// };
+// app.use(tempUserIdInsertion);
 
-app.use(tempUserIdInsertion);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(helmet());
-app.use('/users', userRouter);
-app.use('/cards', cardsRouter);
+
 app.post('/signup', createUser);
 app.post('/signin', login);
+
+app.use(auth);
+app.use('/users', userRouter);
+app.use('/cards', cardsRouter);
+
 app.use('*', pageNotFound);
 
 app.listen(PORT, () => {
