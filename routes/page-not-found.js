1b25f64c77a6c72de06c869df44380e8c4801503
Ivan Lev <fanatos@mail.ru>
2024-01-08 21:45:35 +0500

    style: delete comment lines

--- routes/page-not-found.js
+++ routes/page-not-found.js
@@ -2,8 +2,6 @@ const router = require('express').Router();
 
 const NotFoundError = require('../errors/not-found-error');
 
-router.patch('/', (req, res, next) => next(new NotFoundError('Страница не найдена')),
-  // res.status(404).send({ message: 'Страница не найдена' });
-);
+router.patch('/', (req, res, next) => next(new NotFoundError('Страница не найдена')));
 
 module.exports = router;
