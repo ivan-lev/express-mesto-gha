b4f42b667658d27b05f2e711f59c7e32fce2adbd
Ivan Lev <fanatos@mail.ru>
2024-01-14 18:35:45 +0500

    feat: add cors middleware

--- app.js
+++ app.js
@@ -4,30 +4,35 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
+require('dotenv').config();
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-
 const { validateJoiSignup, validateJoiSignin } = require('./middlewares/joi-users-validation');
+const { cors } = require('./middlewares/cors');
 
 const { createUser, login } = require('./controllers/users');
 
 const auth = require('./middlewares/auth');
 const userRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
-
 const pageNotFound = require('./routes/page-not-found');
+
 const { errorHandler } = require('./middlewares/error-handler');
 
 const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
 
-const { PORT = 3000 } = process.env;
+const {
+  PORT = 3000,
+  DB_URL = 'mongodb://localhost:27017/mestodb',
+} = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect(DB_URL);
 
 app.use(requestLogger);
 
+app.use(cors);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(helmet());
